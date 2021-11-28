4975cb1fce17d18e2c7f584a6edc644d4c18a0bb
github user name <ektotti@gmail.com>
2021-11-28 21:29:02 +0900

    PHP課題①-2initial commit

---
+++ work1-2/index.php
@@ -0,0 +1,34 @@
+<?php
+$fruits = ['apple', 'orange', 'strawberry'];
+
+function judge($answer, $fruits){
+    $result = in_array($answer, $fruits);
+    $judge = $result? $answer.'は配列$fruitsに含まれています': $answer.'は配列$fruitsに含まれていません';
+    echo $judge;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題①−2</title>
+</head>
+<body>
+    <form method="POST">
+    <input type="text" name="answer">
+    <input type="submit" value="OK">
+    </form>
+    <?php 
+
+    if(isset($_POST['answer'])){
+        $answer = $_POST['answer'];
+        judge($answer, $fruits);
+    }
+
+    ?>
+</body>
+</html>
