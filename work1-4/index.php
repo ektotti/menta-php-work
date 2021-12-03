509ffe993da58376e0fe2d6acb02c6d619c892f4
github user name <ektotti@gmail.com>
2021-12-03 15:51:56 +0900

    課題①ー4修正提出-1

--- work1-4/index.php
+++ work1-4/index.php
@@ -1,29 +1,3 @@
-<?php
-$array = [
-    0=>'グー', 
-    1=>'チョキ',
-    2=>'パー'
-    ];
-
-$pcChoice = array_rand($array, 1); 
-
-function judge($myChoice, $pcChoice){
-    switch(($myChoice - $pcChoice + 3)%3){
-    case 0:
-        echo "あいこ";
-        break;
-    case 2:
-        echo "あなたの勝ちです";
-        break;
-    case 1:
-        echo "あなたの負けです";
-        break;
-    }
-
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,22 +8,48 @@ function judge($myChoice, $pcChoice){
 </head>
 <body>
     <form method="POST">
-    <select name="myChoice" id="myChoice">
-        <?php foreach($array as $key => $value): ?>
-            <option value="<?php echo $key ?>"><?php echo $value ?></option>
-        <?php endforeach; ?>
-    </select>
-    <input type="submit" value="OK">
+        <select name="userChoice" id="userChoice">
+                <option value=0>グー</option>
+                <option value=1>チョキ</option>
+                <option value=2>パー</option>
+        </select>
+        <input type="submit" name="submit" value="OK">
     </form>
-    <?php 
-
-    if(isset($_POST['myChoice'])){
-        $myChoice = $_POST['myChoice'];
-        echo "<div>自分:{$array[$myChoice]}</div>" ;
-        echo "<div>相手:{$array[$pcChoice]}</div>" ;
-        judge($myChoice, $pcChoice);
-    }
-
-    ?>
 </body>
 </html>
+
+<?php
+
+function battle($user){
+    $items = ['グー', 'チョキ', 'パー'];
+    $pc = array_rand($items, 1); 
+    $judge = '';
+    $win = 'あなたの勝ちです';
+    $lose = 'あなたの負けです・・・・';
+
+    echo "<div>自分：".$items[$user]."</div>";
+    echo "<div>相手：".$items[$pc]."</div>";
+
+    switch($user){
+    case $pc:
+        $judge = "あいこ";
+        break;
+    case 0:
+        $judge = $pc === 1? $win : $lose;
+        break;
+    case 1:
+        $judge = $pc === 2? $win : $lose;
+        break;
+    case 2:
+        $judge = $pc === 0? $win : $lose;
+        break;
+    }
+
+    echo $judge;
+}
+
+if(isset($_POST['submit'])){
+    $user = (int) $_POST['userChoice'];
+    battle($user);
+}
+?>
