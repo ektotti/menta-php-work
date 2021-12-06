30c96b9eb946ef6701ae33e8b9dc98a1db3d4879
github user name <ektotti@gmail.com>
2021-12-06 22:25:01 +0900

    コメント対応と最終提出

--- work2/work2-1.php
+++ work2/work2-1.php
@@ -16,7 +16,7 @@
     <br>
     <div>【出力】</div>
     <br>
-    <?php if(isset($_POST['inputnum'])){
+    <?php if(isset($_POST['inputnum'])) {
         $fizzNum = (int) $_POST['fizzNum'];
         $buzzNum = (int) $_POST['buzzNum'];
         playFizzBuzz($fizzNum,$buzzNum);
@@ -26,46 +26,45 @@
 </html>
 
 <?php 
-function playFizzBuzz($fizzNum,$buzzNum){
+function playFizzBuzz($fizzNum,$buzzNum) {
     
     // 引数で渡ってきた値のチェック。
-    if(!checkValFromForm($fizzNum, $buzzNum)){
+    if(!checkValFromForm($fizzNum, $buzzNum)) {
         return;
     }
 
     // ループカウンタ変数を１〜100で設定して、入力されたfizzNumとbuzzNumで剰余算を行う
     // 割り切れる数を出力。fizzNumとbuzzNum両方で割り切れる場合はfizzbuzzとする。
-    for($i = 1; $i < 100; $i++){
-        if($i % $fizzNum === 0 && $i % $buzzNum === 0){
+    for($i = 1; $i < 100; $i++ ){
+        if($i % $fizzNum === 0 && $i % $buzzNum === 0) {
 
             echo "fizzbuzz {$i}<br>";
 
-        }elseif($i % $fizzNum === 0){
+        }elseif($i % $fizzNum === 0) {
 
             echo "fizz {$i}<br>";
 
-        }elseif($i % $buzzNum === 0){
+        }elseif($i % $buzzNum === 0) {
 
             echo "buzz {$i}<br>";
 
         }
     }
-    
 }
 
 // formで入力された値のチェックをする関数。
-function checkValFromForm($fizzNum, $buzzNum){
+function checkValFromForm($fizzNum, $buzzNum) {
     $isOk = false;
     
-    if(empty($fizzNum)|empty($buzzNum)){
+    if(empty($fizzNum)|empty($buzzNum)) {
 
         echo '値を入力して下さい';
 
-    }elseif(is_float($fizzNum)|is_float($buzzNum)){
+    }elseif(is_float($fizzNum)|is_float($buzzNum)) {
 
         echo '整数値を入力して下さい';
 
-    }elseif(is_string($fizzNum)){
+    }elseif(is_string($fizzNum)) {
 
         echo '整数値を入力して下さい';
 
