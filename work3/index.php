e02183efcff862b3a3bf6cc8ddef71526c89dc9d
github user name <ektotti@gmail.com>
2021-12-07 23:40:41 +0900

    work3 submit-1

--- work3/index.php
+++ work3/index.php
@@ -1,1 +1,55 @@
-index.php
+<?php
+// このアプリ用のデータベース作成。
+require_once 'pdo.php';
+db\createDatabase();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>work3</title>
+        <style>
+            ul{
+                list-style: none;
+                border: 5px double teal;
+                padding: 5px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>掲示板</h1>
+        <section class="create">
+            <h2>新規投稿</h2>
+            <?php //formからの入力をcontroller.phpで受けて条件分岐。そしてdbの操作 ?> 
+            <form action="controller.php" method="POST">
+                <div>
+                    <label for="name">name:</label>
+                    <input type="text" name="name" id="name">
+                </div>
+                <div>
+                    <label for="postContent" style="display:block;">投稿:</label>
+                    <textarea name="postContent" id="postContent" cols="30" rows="10"></textarea>
+                </div>
+                <input type="submit" name="type" value="投稿">
+            </form>
+        </section>
+        <section clas="show">
+            <h2>投稿内容一覧</h2>
+            <?php 
+            $posts = db\fetchAll();
+            if($posts):
+                foreach($posts as $post):
+            ?>
+                <ul>
+                    <li><?php echo "No:".$post['id'] ?></li>
+                    <li><?php echo "名前:".$post['name'] ?></li>
+                    <li><?php echo "投稿内容:".$post['content'] ?></li>
+                </ul>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </section>
+    </body>
+</html>
---
+++ work3/pdo.php
@@ -0,0 +1,34 @@
+<?php 
+namespace db;
+use PDO;
+
+function createDatabase() {
+    $db = new PDO('mysql:host = localhost;','root','root');
+    $db->exec('CREATE DATABASE IF NOT EXISTS test_bbs;');
+    $db->exec('CREATE TABLE IF NOT EXISTS test_bbs.post(
+    `id` int auto_increment primary key,
+    `name` VARCHAR(10) default "root" ,
+    `content` VARCHAR(140) NOT NULL);
+    ');
+}
+
+function insert($name, $content) {
+    $db = new PDO('mysql:host = localhost;dbname=test_bbs','root','root');
+    $statement = $db->prepare('insert into test_bbs.post(`name`, `content`) values(:name, :content); ');
+    $statement->execute(
+        [
+            ':name'=>$name,
+            ':content'=>$content
+        ]
+        );
+}
+
+function fetchAll() {
+    $db = new PDO('mysql:host = localhost;dbname=test_bbs','root','root');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $statement = $db->prepare('select * from test_bbs.post;');
+    $statement->execute();
+    return $statement->fetchAll();
+    
+}
+?>
